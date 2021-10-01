8e0153b9d68346e09497615bb36b7b15cb491a47
yasminemediamacs <yasmine@mediamacs.com>
2021-10-01 19:29:43 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Yasmine Senter and is{" "}
-        <a
-          href="https://github.com/yasminemediamacs/react-weather"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by <a href="">Yasmine Senter</a> and is{" "}
+          <a
+            href="https://github.com/yasminemediamacs/react-weather"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
