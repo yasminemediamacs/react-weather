688ac2ab7e3502bd067e9fe6b5cd2e20a641286b
yasminemediamacs <yasmine@mediamacs.com>
2021-10-01 19:18:19 +0200

    Added the heading and updated the Github repo Url

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
-        This project is created by Yasmine Senter and is
+        This project is created by Yasmine Senter and is{" "}
         <a
           href="https://github.com/yasminemediamacs/react-weather"
           target="_blank"
